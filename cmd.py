c543d5261951a47b1f73f39d4e91c5906e787941
Fryy56 <3pm.fryy@gmail.com>
2025-01-02 01:47:40 +0500

    Update *.py files
    - Lint both files
    - Small improvements to both files
    - Add docs for extra `help` functions
    - Add arg_test function to cmd.py for argument number checks

--- cmd.py
+++ cmd.py
@@ -1,4 +1,26 @@
-# exit
-def exit_cmd():
-    exit(0)
+""" Custom module responsible for commands """
+import sys
+from colorama import Fore
 
+
+def arg_test(arg_num, command):   # pylint: disable=missing-function-docstring
+    if len(command) - 1 == arg_num:
+        return True
+    print(Fore.YELLOW + "Подано", len(command) - 1,
+          "аргументов, ожидалось:", arg_num)
+    return False
+
+
+def exit_cmd():
+    """ Завершает выполнение программы. """
+    sys.exit()
+
+
+def help_cmd(*args):
+    """ Выводит список всех команд сервиса/описание заданной команды (при
+    поданном аргументе).
+
+    help [%command%]
+
+        %command% - команда для получения справки.
+    """
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Modules
-from colorama import Fore
+# Modules   pylint: disable=missing-module-docstring
 import cmd
+from colorama import init, Fore
 
-while True: # Main input loop
-    command = input(Fore.RESET + ">>> ")
 
-    match command:
+init()   # Initialize colorama for color compatibility
+while True:   # Main input loop
+    command = input(Fore.RESET + ">>> ").split()
+    command_header = command[0]
+    match command_header:
         case "exit":
-            cmd.exit_cmd()
+            if cmd.arg_test(0, command):
+                cmd.exit_cmd()
         case _:
-            print(Fore.RED + "'", command, "' - не команда сервиса. Используйте `help` для списка команд.", sep='')
+            print(Fore.RED + "'", command_header, "' - не команда сервиса. "
+                  "Используйте `help` для списка команд.", sep='')
